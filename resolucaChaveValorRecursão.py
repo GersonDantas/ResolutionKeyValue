f646a276afc2b578c5f0d9ea9c7f46e3c49d8d76
Gerson Dantas <53773325+GersonDantas@users.noreply.github.com>
2021-06-05 09:04:21 -0300

    Update resolucaChaveValorRecursão.py

--- resolucaChaveValorRecursão.py
+++ resolucaChaveValorRecursão.py
@@ -8,14 +8,16 @@ command = ["d", "4","d", "4","d", "4","a", "1", "b", "2", "c", "3", "d", "4","s"
 pair = []
 inicio = time.time()
 def group_pars(arr):
+    # Este é o caso de parada
+    if len(arr) == 2:
+        return pair.append(arr) 
+    #Aqui a função faz o tratamento para o caso de números de elementos forem impares
     if len(arr) % 2 != 0:
         imp = len(arr) -1
         pair.append([arr[len(arr)-1]])
         arr.pop(imp)
         group_pars(arr)
         return -1
-    if len(arr) == 2:
-        return arr
     if len(arr)/2 % 2 != 0:
         center_min = len(arr)//2 -1
         center_max = len(arr)//2 
